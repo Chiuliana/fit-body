8df026aeecb6b01ed43cdd4519dd74b7fbd66b6e
Chichioi Iuliana <chichioiuliana@gmail.com>
2025-06-04 15:48:42 +0300

    configure project to work when deployed

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router';
 import routes from './routes';
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+  basename: '/fit-body/',
+});
 
 export default router;
